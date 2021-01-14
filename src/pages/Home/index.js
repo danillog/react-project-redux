acfd099fba59f51cc0a7849e33b86d0fc1ea8030
danillog <danillog821@gmail.com>
2021-01-14 10:36:49 -0300

    adicionando estilo

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { MdFlightTakeoff } from 'react-icons/md';
+import api from '../../services/api';
+import './style.css';
 
 export default function Home(){
+  const [trips, setTrips] = useState([]);
+
+
+  useEffect(() => {
+
+    async function loadAPI(){
+      const response = await api.get('trips');
+      setTrips(response.data);
+    }
+
+    loadAPI();
+  }, []);
+
   return(
-    <div>
-      <h1> Home </h1>
+    <div className="box">
+      {trips.map(trip => (
+        <li key={trip.id}>
+          <img src={trip.image}  alt="Foto do local" />
+           <strong> { trip.title } </strong>
+           <span> Status: {trip.status ? 'Disponível' : "Indisponível "} </span> 
+
+          <button type="button" onClick={ () => {} }> 
+            <MdFlightTakeoff  size={16} color="#fff"/>
+            <span> Solicitar Reseva </span> 
+          </button> 
+        </li>
+
+      ))}
     </div>
   );
 }
--- src/pages/Reservas/index.js
+++ src/pages/Reservas/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { MdDelete } from 'react-icons/md';
+import './style.css';
 
 export default function Reservas(){
   return(
     <div>
-      <h1> Reservas </h1>
+      <h1> Vocẽ solicitou 1 reserva </h1>
+      
+      <div className="reservas">
+        <img src="https://sujeitoprogramador.com/wp-content/uploads/2019/12/maceio.jpg"
+        alt="Imagem de praia em MAceió" />
+        
+        <strong> Viagem maceio 7 dias </strong>
+        <span> Quantidade: 2 </span>
+        <button 
+          type= "button"
+          onClick={ () => {} }>
+        <MdDelete size={20} color="#fff" /> 
+        </button>
+      </div>
+      <footer>
+        <button type="button"> Solicitar Reservas </button>
+      </footer>
+
     </div>
   );
 }
